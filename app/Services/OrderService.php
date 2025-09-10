7da77710f8a00871134e4b6da48c75abfecd9d6a
Михаил <tumbashka@gmail.com>
2025-09-10 13:16:42 +0400

    Update create order logic

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,17 +2,23 @@
 
 namespace App\Services;
 
+use App\Data\AdditionalServiceData;
 use App\Data\OrderData;
 use App\Data\OrderPaymentData;
 use App\Enums\OrderStatus;
 use App\Enums\PaymentType;
+use App\Enums\ProductType;
 use App\Interfaces\PaymentInterface;
-use App\Models\AdditionalService;
-use App\Models\Car;
 use App\Models\Order;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 class OrderService
 {
+    /**
+     * @throws Throwable
+     */
     public function createOrderAndGetPaymentURL(OrderData $orderDTO): string
     {
         $order = $this->createOrder($orderDTO);
@@ -28,45 +34,49 @@ class OrderService
         return $payment->url;
     }
 
+    /**
+     * @throws Throwable
+     */
     public function createOrder(OrderData $orderDTO): Order
     {
-        $order = Order::create([
-            'status' => OrderStatus::NeedPayment->value,
-            'rent_start' => $orderDTO->start_date,
-            'rent_end' => $orderDTO->end_date,
-            'user_id' => $orderDTO->user_id,
-        ]);
+        return DB::transaction(function () use ($orderDTO) {
+            $order = Order::create([
+                'status' => OrderStatus::NeedPayment->value,
+                'rent_start' => $orderDTO->start_date,
+                'rent_end' => $orderDTO->end_date,
+                'user_id' => $orderDTO->user->id,
+            ]);
 
-        $order->cars()
-            ->attach(
-                $orderDTO->car_id,
-                [
-                    'price' => $orderDTO->car->getRawOriginal('price'),
-                    'name' => $orderDTO->car->name,
-                ]
-            );
+            $order->products()->create([
+                'price' => $orderDTO->car->price,
+                'name' => $orderDTO->car->name,
+                'product_id' => $orderDTO->car->id,
+                'product_type' => ProductType::Car,
+            ]);
 
-        $services = AdditionalService::findMany($orderDTO->services);
+            $services = $orderDTO->services;
+            /** @var $service AdditionalServiceData */
+            foreach ($services as $service) {
+                $order->products()->create([
+                    'price' => $service->price,
+                    'name' => $service->name,
+                    'product_id' => $service->id,
+                    'product_type' => ProductType::AdditionalService,
+                ]);
+            }
 
-        foreach ($services as $service) {
-            $order->additionalServices()
-                ->attach(
-                    $service,
-                    [
-                        'price' => $service->getRawOriginal('price'),
-                        'name' => $service->name,
-                    ]
-                );
-        }
-
-        return $order;
+            return $order;
+        });
     }
 
+    /**
+     * @throws Exception
+     */
     private function getPaymentService($paymentId): PaymentInterface
     {
         return match ($paymentId) {
             PaymentType::Yookassa->value => new YookassaService,
-            default => throw new \Exception("Not supported payment service: {$paymentId}"),
+            default => throw new Exception("Not supported payment service: {$paymentId}"),
         };
     }
 
