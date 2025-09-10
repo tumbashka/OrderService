00f6618ce6259a67e4a2bb1f59ab0d478b53dddf
Михаил <tumbashka@gmail.com>
2025-09-10 13:26:38 +0400

    remove imports

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use App\Enums\OrderStatus;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class Order extends Model
 {
